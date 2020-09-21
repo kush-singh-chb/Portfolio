831c42412e29835e64e039266b3e5989e8696392
Kush Singh Chibber <kushsinghraghvinder.chibber@student.griffith,ie>
2020-09-21 20:07:45 +0100

    added jquery

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+var path    = require('path');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname +path.sep+ 'js'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.render('pages/index');
 });
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
