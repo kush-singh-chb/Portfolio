1dd32c24a7c754c136f8a98e7914342aec7cd1da
Kush Singh Chibber <kushsinghraghvinder.chibber@student.griffith,ie>
2020-09-27 12:01:22 +0100

    dockerfile

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-var express = require('express');
-var app = express();
-var path    = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname +path.sep+ 'js'));
-
+const publicDir = path.join(__dirname, '/public');
+app.use(express.static(publicDir));
 app.get('/', function (req, res) {
+    res.render('pages/index');
+});
+app.post('/contactForm', function (req, res) {
+console.log('received form details')
     res.render('pages/index');
 });
 app.listen(3000, function () {
